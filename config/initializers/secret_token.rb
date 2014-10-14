16db7dd819b05c9686e9a19f9c3652daec109e50
fedet <federico.turchetti@gmail.com>
2014-10-14 17:38:41 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsqlApp::Application.config.secret_token = '2cb6563ddb5f695fb096a53a87b62350d2b9e30710cbba5464dafc4ed1d3a1f662fd92892f2ae8e29d8846da3e9f631a99df05678b635daf7366bdfede4c6ce7'
